3113577c261ee48596b1f98a8fcc70c797eb1c5e
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2024-08-26 16:44:44 -1000

    navbar update

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,7 +13,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="w-screen h-screen bg-gradient-to-b from-teal-50 to-orange-50">
+        <div className="w-screen h-screen bg-gradient-to-t from-orange-100 from-0% via-amber-50 from-30% to-sky-200 to-100%">
           <div className="h-24">
             <Navbar />
           </div>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,29 +1,43 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
 import Image from "next/image";
 import "./style.css";
 
+const links = [
+  { url: "/", text: "Home" },
+  { url: "/about", text: "About" },
+  { url: "/portfolio", text: "Portfolio" },
+  { url: "/projects", text: "Projects" },
+  { url: "/contact", text: "Contact" },
+];
+
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
       {/* LINKS */}
-      <div className="flex items-center justify-center">
-        <Link href="/" className="text-sm text-black mx-5">
-          Home
-        </Link>
-        <Link href="/" className="text-sm text-black mx-5">
-          About
-        </Link>
-        <Link href="/" className="text-sm text-black mx-5">
-          Projects
-        </Link>
+      <div className="hidden md:flex">
+        {links.map((link) => (
+          <Link
+            href={link.url}
+            key={link.text}
+            className="text-sm text-black mx-5"
+          >
+            {link.text}
+          </Link>
+        ))}
       </div>
+
       {/* LOGO */}
+      <div className="md:hidden w-1 h-1"></div>
       <div className="anim flex items-center justify-center">
         <span class="mirror anim-text-mirror text-sm">ristine</span>
         <Link href="/" className="anim-btn">
           <Image
             className="origin-center -rotate-90 hover:rotate-0 transition duration-500 ease-in-out cursor-pointer"
-            src="https://raw.githubusercontent.com/Kristine-Rivera/Profile/main/public/logo.png"
+            src="/logo.png"
             alt="logo"
             width={30}
             height={30}
@@ -31,8 +45,36 @@ const Navbar = () => {
         </Link>
         <span class="anim-text text-sm">ivera</span>
       </div>
+
+      {/* ICONS */}
+      <div className="hidden md:flex">
+        <Link href="/">
+          <Image src="/github.png" alt="" width={24} height={24} />
+        </Link>
+      </div>
+
       {/* RESPONSIVE MENU */}
-      <div className="">2</div>
+      <div className="md:hidden">
+        {/* MENU BUTTON */}
+        <button
+          classname="w-7 h-8 flex flex-col justify-between"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          <div className="z-50 relative w-7 h-1 m-1 bg-white rounded"></div>
+          <div className="z-50 relative w-7 h-1 m-1 bg-white rounded"></div>
+          <div className="z-50 relative w-7 h-1 m-1 bg-white rounded"></div>
+        </button>
+        {/* MENU LIST */}
+        {open && (
+          <div className="absolute top-0 left-0 w-full h-full bg-black text-white flex flex-col items-center justify-center gap-8 text-2xl">
+            {links.map((link) => (
+              <Link href={link.url} key={link.text}>
+                {link.text}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
