a63125a9a9dc0a49bd2890ddcdaaad2c74f553fd
Kristine Rivera <40307136+tineriver@users.noreply.github.com>
2024-09-01 13:20:36 -1000

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
         <Link href="/" className="anim-btn">
           <Image
             className="origin-center -rotate-90 hover:rotate-0 transition duration-500 ease-in-out cursor-pointer"
-            src="/logo.png"
+            src="https://raw.githubusercontent.com/Kristine-Rivera/Profile/main/public/logo.png"
             alt="logo"
             width={30}
             height={30}
@@ -48,8 +48,13 @@ const Navbar = () => {
 
       {/* ICONS */}
       <div className="hidden md:flex">
-        <Link href="/">
-          <Image src="/github.png" alt="" width={24} height={24} />
+        <Link href="https://github.com">
+          <Image
+            src="https://raw.githubusercontent.com/Kristine-Rivera/Profile/main/public/github.png"
+            alt=""
+            width={24}
+            height={24}
+          />
         </Link>
       </div>
 
